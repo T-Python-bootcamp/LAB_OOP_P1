7310eabf92ec6e84f97354fba1ab341b141b3296
RawanZaben <rawan.zaben.rae.js@tuwaiq.edu.sa>
2022-02-23 14:00:58 +0300

    lap oop

---
+++ oop.py
@@ -0,0 +1,29 @@
+class Bank:
+    def __init__(self,AccountNumber, owner, balance):
+        self.AccountNumber = AccountNumber
+        self.owner = owner
+        self.balance = balance
+
+    def Deposit(self,money ):
+        self.balance += money 
+        print("Deposit :{}".format(money))
+
+    def Withdrawal(self,money ):
+        if self.balance > money :
+            self.balance -= money 
+            print("Withdrawal :{}".format(money))
+
+    def bankFees(self):
+        Fees=self.balance*93/100
+        print("Fees :{}".format(Fees))
+
+    def display(self):
+        print("Account Number : {} , owner : {} , balance : {}  ...".format(self.AccountNumber,self.owner, self.balance))
+
+################################
+
+user1=Bank(3456789,"Ahmed",3000)
+user1.Deposit(200)
+user1.Withdrawal(100)
+user1.bankFees()
+user1.display() 
